4f5dbb80a399e87e2d3ce9f0befe03633f8417b8
geesawra <hello@geesawra.industries>
2025-10-02 14:13:00 +0200

    ComposeView: paste images as attachments

--- app/src/main/java/industries/geesawra/jerryno/ComposeView.kt
+++ app/src/main/java/industries/geesawra/jerryno/ComposeView.kt
@@ -7,8 +7,14 @@ import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.content.MediaType
+import androidx.compose.foundation.content.ReceiveContentListener
+import androidx.compose.foundation.content.consume
+import androidx.compose.foundation.content.contentReceiver
+import androidx.compose.foundation.content.hasMediaType
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,7 +31,9 @@ import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.clearText
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
@@ -45,25 +53,22 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import industries.geesawra.jerryno.datalayer.SkeetData
 import industries.geesawra.jerryno.datalayer.TimelineViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun ComposeView(
     context: Context,
@@ -75,11 +80,12 @@ fun ComposeView(
 ) {
     val focusRequester = remember { FocusRequester() }
     val keyboardController = LocalSoftwareKeyboardController.current
-    var postText by remember { mutableStateOf("") }
     val charCount = remember { mutableIntStateOf(0) }
     val wasEdited = remember { mutableStateOf(false) }
     val maxChars = 300
-
+    val composeFieldState = rememberTextFieldState(
+        ""
+    )
     val mediaSelected = remember { mutableStateOf(mapOf<Uri, String?>()) }
     val mediaSelectedIsVideo = remember { mutableStateOf(false) }
 
@@ -90,12 +96,29 @@ fun ComposeView(
         } else {
             keyboardController?.hide()
             // Reset state when sheet is hidden
-            postText = ""
+            composeFieldState.clearText()
             charCount.intValue = 0
             inReplyTo.value = null
             mediaSelected.value = mapOf()
             mediaSelectedIsVideo.value = false
 
+        }
+    }
+
+    // Remember the ReceiveContentListener object as it is created inside a Composable scope
+    val receiveContentListener = remember {
+        ReceiveContentListener { transferableContent ->
+            when (transferableContent.hasMediaType(MediaType.Image)) {
+                true -> transferableContent.consume {
+                    val uri = it.uri
+                    val mimeType: String? = context.contentResolver.getType(uri)
+                    mediaSelected.value = mapOf(Pair(uri, mimeType))
+                    true
+                }
+
+
+                false -> transferableContent
+            }
         }
     }
 
@@ -182,28 +205,26 @@ fun ComposeView(
                     }
                 }
 
-                OutlinedTextField(
-                    keyboardActions = KeyboardActions(
-                        onDone = {
-                            this.defaultKeyboardAction(ImeAction.Done)
-                            keyboardController?.hide()
-                        }
-                    ),
-                    value = postText,
-                    onValueChange = {
+                LaunchedEffect(composeFieldState.text) {
+                    if (composeFieldState.text.isEmpty()) {
+                        wasEdited.value = false
+                    } else {
                         wasEdited.value = true
-                        postText = it
-                        charCount.intValue = it.length
-                    },
+                        charCount.intValue = composeFieldState.text.length
+                    }
+                }
+
+                OutlinedTextField(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .heightIn(min = 250.dp) // Adjusted min height for text field
-                        .focusRequester(focusRequester),
+                        .heightIn(min = 250.dp)
+                        .focusRequester(focusRequester)
+                        .contentReceiver(receiveContentListener),
                     label = {
                         if (wasEdited.value) {
                             Text(
                                 text = "${maxChars - charCount.intValue}",
-                                color = if (postText.length > maxChars) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.onSurface
+                                color = if (composeFieldState.text.length > maxChars) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.onSurface
                             )
                         } else {
                             Text(
@@ -211,15 +232,16 @@ fun ComposeView(
                             )
                         }
                     },
-                    isError = postText.length > maxChars,
-                    maxLines = 10
+                    isError = composeFieldState.text.length > maxChars,
+                    lineLimits = TextFieldLineLimits.MultiLine(maxHeightInLines = 10),
+                    state = composeFieldState
                 )
 
                 ActionRow(
                     context,
                     uploadingPost,
                     pickMedia,
-                    postText,
+                    composeFieldState.text.toString(),
                     mediaSelected,
                     mediaSelectedIsVideo,
                     coroutineScope,
