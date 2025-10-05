3629609b7bb207819b8ce4aaf61bdfc8108cee0b
geesawra <hello@geesawra.industries>
2025-10-05 15:29:40 +0200

    *: try building with fastlane

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_24
+        targetCompatibility = JavaVersion.VERSION_24
     }
     compileOptions {
-        version = JavaVersion.VERSION_21.toString()
+        version = JavaVersion.VERSION_24.toString()
     }
     buildFeatures {
         compose = true
