758f800fe5f55d8f94dc2ccda78590d59ede7332
geesawra <hello@geesawra.industries>
2025-10-21 15:41:32 +0200

    Lists: remove loading indicator
    Removes the `CircularProgressIndicator` that was previously shown at the bottom of the timeline and notifications lists while fetching more items.

--- app/src/main/java/industries/geesawra/monarch/NotificationsView.kt
+++ app/src/main/java/industries/geesawra/monarch/NotificationsView.kt
@@ -1,24 +1,18 @@
 package industries.geesawra.monarch
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import industries.geesawra.monarch.datalayer.Notification
@@ -66,27 +60,6 @@ fun NotificationsView(
                     notification = notif,
                     onReplyTap = onReplyTap
                 )
-            }
-        }
-
-        if (viewModel.uiState.isFetchingMoreNotifications && viewModel.uiState.notifications.isNotEmpty()) {
-            item {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Box(
-                        contentAlignment = Alignment.Center,
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        CircularProgressIndicator(
-                            modifier = Modifier
-                                .width(64.dp),
-                        )
-                    }
-                }
             }
         }
     }
